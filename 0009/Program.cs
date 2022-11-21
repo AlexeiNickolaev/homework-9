86176fe496a41e2dfad648f3f9e1d5ff48e779e6
Alexei <datlife2015@yandex.ru>
2022-11-21 14:07:30 +0300

    Найдена сумма элементов

--- 0009/Program.cs
+++ 0009/Program.cs
@@ -4,6 +4,12 @@ string NaturalsNumbers(int M, int N)
     if(M<=N)return NaturalsNumbers(M, N-1)+$"{N} ";
     else return" ";
 }
+// Найти сумму элементов от M до N, N и M заданы
+int SumElements(int M, int N)
+{
+    if(M==N)return 0;
+    else return N+SumElements(M, N-1);
+}
 
 Console.WriteLine("Показать натуральные числа от M до N");
 Console.Write("Введите число M: ");
@@ -12,3 +18,8 @@ Console.Write("Введите число N: ");
 int N = int.Parse(Console.ReadLine() ?? "0");
 NaturalsNumbers(M, N);
 Console.WriteLine(NaturalsNumbers(M, N));
+
+int sum = SumElements( M, N);
+Console.WriteLine($"Сумма элементов от {M} до {N} равна {sum}");
+Console.ReadKey();
+Console.Clear();
