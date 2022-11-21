e6b72e629cbccad6252872a997f0e3f056e66873
Alexei <datlife2015@yandex.ru>
2022-11-21 14:04:11 +0300

    Показаны натуральные числа от M до N

---
+++ 0009/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+string NaturalsNumbers(int M, int N)
+{
+    if(M<=N)return NaturalsNumbers(M, N-1)+$"{N} ";
+    else return" ";
+}
+
+Console.WriteLine("Показать натуральные числа от M до N");
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+NaturalsNumbers(M, N);
+Console.WriteLine(NaturalsNumbers(M, N));
